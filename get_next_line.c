f55d6b8a94efa7544fdf5e2d0f64ca40e93e295c
Mathis <mjouot@student.42angouleme.fr>
2022-10-13 12:53:07 +0200

    idk

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mjouot <mjouot@marvin.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 14:50:48 by mjouot            #+#    #+#             */
-/*   Updated: 2022/10/12 01:06:51 by mjouot           ###   ########.fr       */
+/*   Updated: 2022/10/13 12:22:44 by mjouot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,19 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-char	*ft_line_alloc
+char	*ft_line_alloc(char *line)
+{
+	
+}
 
 char *get_next_line(int fd)
 {
 	size_t	i;
 	char	*line;
 	char	*tmp;
-	static char buf[BUFFER_SIZE + 1] = "";	
+	static char buf[BUFFER_SIZE + 1] = "";	:w
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (buf == NULL)
 		return (NULL);
@@ -33,12 +36,9 @@ char *get_next_line(int fd)
 		buf = read(fd, buf, BUFFER_SIZE);
 		if (ret == -1)
 			return (NULL);
-		while (buf[i] != '\n' && i < ret)
-		{
-			if (buf[i] == '\n')
-				tmp = ft_substr(buf, 0, i);
-			//cat line+tmp;
-		i++;
+		
+			
+			i++;
 		}
 	
 	}
